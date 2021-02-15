066b01f7a9258d70dc25db54ba9382ae07c8961c
Daniel Mack <drmterps@yahoo.com>
2021-02-15 15:54:54 -0500

    Remove non-exhaustive impls

--- src/core_impls.rs
+++ src/core_impls.rs
@@ -1,4 +1,4 @@
-use core::{cmp::Ordering, fmt::Alignment, num::FpCategory, sync::atomic};
+use core::{cmp::Ordering, fmt::Alignment, num::FpCategory};
 
 use super::*;
 
@@ -6,11 +6,4 @@ foreign_derive_var_iter! {
     Ordering [Ordering::Greater, Ordering::Less, Ordering::Equal]
     Alignment [Alignment::Center, Alignment::Right, Alignment::Left]
     FpCategory [FpCategory::Nan, FpCategory::Infinite, FpCategory::Zero, FpCategory::Subnormal, FpCategory::Normal]
-    atomic::Ordering [
-        atomic::Ordering::Relaxed,
-        atomic::Ordering::Release,
-        atomic::Ordering::Acquire,
-        atomic::Ordering::AcqRel,
-        atomic::Ordering::SeqCst,
-    ]
 }
--- src/std_impls.rs
+++ src/std_impls.rs
@@ -1,5 +1,4 @@
 use std::{
-    io::ErrorKind,
     net::Shutdown,
     sync::mpsc::{RecvTimeoutError, TryRecvError},
 };
@@ -7,26 +6,6 @@ use std::{
 use super::*;
 
 foreign_derive_var_iter! {
-    ErrorKind [
-        ErrorKind::NotFound,
-        ErrorKind::PermissionDenied,
-        ErrorKind::ConnectionRefused,
-        ErrorKind::ConnectionReset,
-        ErrorKind::ConnectionAborted,
-        ErrorKind::NotConnected,
-        ErrorKind::AddrInUse,
-        ErrorKind::AddrNotAvailable,
-        ErrorKind::BrokenPipe,
-        ErrorKind::AlreadyExists,
-        ErrorKind::WouldBlock,
-        ErrorKind::InvalidInput,
-        ErrorKind::InvalidData,
-        ErrorKind::TimedOut,
-        ErrorKind::WriteZero,
-        ErrorKind::Interrupted,
-        ErrorKind::Other,
-        ErrorKind::UnexpectedEof,
-    ]
     Shutdown [Shutdown::Read, Shutdown::Write, Shutdown::Both]
     RecvTimeoutError [RecvTimeoutError::Timeout, RecvTimeoutError::Disconnected]
     TryRecvError [TryRecvError::Empty, TryRecvError::Disconnected]
